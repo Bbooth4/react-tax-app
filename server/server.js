4240366b19851fe898db903dec4deb143c117da2
Brandon <booth.brandon4@gmail.com>
2017-08-01 10:08:37 -0400

    updated

--- server/server.js
+++ server/server.js
@@ -4,6 +4,9 @@ const SocketServer      = require('ws').Server;
 const WebSocket         = require('ws');
 const models            = require("./models");
 const sass              = require("node-sass-middleware");
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { postsReducer } from '../src/reducers/postsReducers.js';
 
 // app.use("/scss", sass({
 //   src: __dirname + "./scss",
@@ -14,73 +17,95 @@ const sass              = require("node-sass-middleware");
 
 require('dotenv').config()
 
-const server = express()
-  .use(express.static('public'))
+const app = express();
+
+app.use('/public', express.static('public'))
   .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${ PORT }`));
+
+// app.use(handleRender)
+
+models.sequelize.sync({ force: false }).then(() => {
+  app.get("/posts", (req, res) => {
+    models.post.findAll({ raw: true })
+      .then((response) => {
+        res.send(response);
+      }).catch((err) => {
+        console.error(err);
+      });
+  });
+});
 
 // Create the WebSockets server
 //**Need to do server: app to use express. */
-const wss = new SocketServer({ server });
+// const wss = new SocketServer({ server });
 
 //Set force : true to synchronize models with postgres db. 
 //This will wipe all the data in database!
-models.sequelize.sync({ force: false }).then(() => {
-  wss.broadcast = (data) => {
-    wss.clients.forEach(function each(client) {
-      if (client.readyState === client.OPEN) {
-        client.send(JSON.stringify(data));
-      }
-    })
-  }
+// models.sequelize.sync({ force: false }).then(() => {
+//   wss.broadcast = (data) => {
+//     wss.clients.forEach(function each(client) {
+//       if (client.readyState === client.OPEN) {
+//         client.send(JSON.stringify(data));
+//       }
+//     })
+//   }
 
-  clientConnected = () => {
-    models.task.findAll().then((data) => {
-      console.log('All tasks')
-      wss.broadcast({ type: 'allTasks', data: data });
-    })
-  }
+//   clientConnected = () => {
+//     models.task.findAll().then((data) => {
+//       console.log('All tasks')
+//       wss.broadcast({ type: 'allTasks', data: data });
+//     })
+//   }
 
-  wss.on('connection', (client) => {
-    console.log(`connection ${client}`);
-    clientConnected();
-    client.on('message', (data) => {
-      data = JSON.parse(data)
-      switch (data.type) {
-        // Path: /users/new Method: POST
-        case 'auth0-login':
-          login(data);
-          break;
-          // Path: /projects/new Method: POST
-          // Path: /users/new Method: POST
-          // Path: /tasks/new Method: POST
-        // case "eventCreation-newProject":
-        //   eventCreation_newProject(data, client);
-        //   break;
-          // Path: /tasks/update Method: PUT
-        default:
-          throw new Error("Unknown event type " + data.type)
-      }
-    });
+//   wss.on('connection', (client) => {
 
-    client.on('close', (event) => {
-      console.log('Client disconnected');
-    });
-  });
+//     models.posts.findAll()
+//     .then((res) => {
+//       console.log(res)
+//     }).catch((err) => {
+//       console.error(err);
+//     })
 
-});
+//     console.log(`connection ${client}`);
+//     clientConnected();
+//     client.on('message', (data) => {
+//       data = JSON.parse(data)
+//       switch (data.type) {
+//         // Path: /users/new Method: POST
+//         case 'auth0-login':
+//           login(data);
+//           break;
+//           // Path: /projects/new Method: POST
+//           // Path: /users/new Method: POST
+//           // Path: /tasks/new Method: POST
+//         // case "eventCreation-newProject":
+//         //   eventCreation_newProject(data, client);
+//         //   break;
+//           // Path: /tasks/update Method: PUT
+//         default:
+//           throw new Error("Unknown event type " + data.type)
+//       }
+//     });
 
-const login = (data, client) => {
-  /* Adds user details to database on login */
-  models.user.count({ where: { email: data.email } }).then((count) => {
-    if (count > 0) {
-      console.log('user exists');
-    } else {
-      models.user.create({
-        first_name: data.first_name,
-        last_name: data.last_name,
-        email: data.email,
-        avatar: data.picture
-      })
-    }
-  })
-}
+//     client.on('close', (event) => {
+//       console.log('Client disconnected');
+//     });
+//   });
+
+// });
+
+// const login = (data, client) => {
+//   /* Adds user details to database on login */
+//   models.user.count({ where: { email: data.email } }).then((count) => {
+//     if (count > 0) {
+//       console.log('user exists');
+//     } else {
+//       models.user.create({
+//         first_name: data.first_name,
+//         last_name: data.last_name,
+//         email: data.email,
+//         avatar: data.picture
+//       })
+//     }
+//   })
+// }
